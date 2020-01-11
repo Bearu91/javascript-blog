39210ddd42ee4ba5575212fa4e840826e52c9386
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2020-01-11 17:57:26 +0100

    add fix after classes but hmtl not working

--- js/script.js
+++ js/script.js
@@ -1,67 +1,72 @@
 'use strict';
 
-/* document.getElementById('test-button').addEventListener('click', function(){
-const links = document.querySelectorAll('.titles a');
-console.log('links:', links);
-}); */
-
-
-
-function generatePostlinks(tag) {
-  let html = '';
-  const posts = document.querySelectorAll('.post')
-  for (let post of posts) {
-    if (tag) {
-      if (post.getAttribute('data-tags').indexOf(tag) > -1) {
-        const title = post.querySelector('.post-title').innerHTML;
-        const id = post.getAttribute('id');
-        const link = '<li><a href="#" class="post-link" data-postId="' + id + '">' + title + '</a></li>';
-        html = html + link;
-    } else {
-      const title = post.querySelector('.post-title').innerHTML;
-      const id = post.getAttribute('id');
-      const link = '<li><a href="#" class="post-link data-postId="' + id + '">' + title + '</a></li>';
-      html = html + link;
-  
-    }
-  }
-
-  const target = document.querySelector('.titles');
-  target.innerHTML = html;
-  const postLinks = document.querySelectorAll('.post-link');
-  for (let postLink of postLinks) {
-    postLink.addEventListener('click', function (event) {
-      event.preventDefault();
-      const id = event.target.getAttribute('data-postId');
-      setActivePost(id);
-    });
-    tagsWrapper.innerHTML = html;
-  }
-  addClickListenersToTags();
+function generateLink(id, title) {
+return '<li><a href="#" class="post-link" data-postId="' + id + '">' + title + '</a></li> ';
 }
+
+
+function generatePostlinks() {
+let html = '';
+const posts = document.querySelectorAll('.post');
+for (let post of posts) {
+let tagsHtml = '';
+const id = post.getAttribute('id');
+const tags = post.getAttribute('data-tags').split(' ')
+const title = post.querySelector('.post-title').innerHTML;
+html = html + generateLink(id, title);
+
+for (let tag of tags) {
+tagsHtml = tagsHtml + generateLink('#', tag);
+}
+const tagsContainer = post.querySelector('.list-horizontal');
+tagsContainer.innerHTML = tagsHtml;
+}
+const target = document.querySelector('.titles');
+target.innerHTML = html;
+const postLinks = document.querySelectorAll('.post-link');
+for (let postLink of postLinks) {
+postLink.addEventListener('click', function (event) {
+event.preventDefault();
+const id = event.target.getAttribute('data-postId');
+setActivePost(id);
+});
+}
+addClickListenersToTags();
+
 function setActivePost(postId) {
-  const posts = document.querySelectorAll('.post');
-  for (let post of posts) {
-    post.classList.remove('active');
-  }
-  document.getElementById(postId).classList.add('active');
+const posts = document.querySelectorAll('.post');
+for (let post of posts) {
+post.classList.remove('active');
 }
-generatePostLinks();
+document.getElementById(postId).classList.add('active');
+}
 
 function addClickListenersToTags() {
-  const tags = document.querySelectorAll('.tags');
-  for (let tag of tags) {
-    tag.addEventListener('cilck', function (event){
-      displayTags(event.target.getAttribute('data-tagId'));
-    })
-  }
+const tags = document.querySelectorAll('.tags');
+for (let tag of tags) {
+tag.addEventListener('cilck', function (event) {
+displayTags(event.target.getAttribute('data-tagId'));
+})
 }
-function generateAuthors () {
-  let html = '';
-  const authors = document.querySelectorAll('.post-author');
-  for (let author of authors){
-    const author = post-author.getAttribute(??)
-    const link = '<li><a href="#" class="post-author" data-postId="' + author + '">' + ?? + '</a></li>';
-    html = html + link;
-  }
 }
+}
+
+function generateAuthors() {
+let html = '';
+const authArr = [];
+const authors = document.querySelectorAll('.post-author');
+for (let author of authors) {
+const aut = author.innerHTML.replace('by ', '');
+if (authArr.indexOf(aut) === -1) {
+authArr.push(aut);
+}
+}
+for (let autor of authArr) {
+html = html + generateLink(null, autor);
+}
+const target = document.querySelector('.authors');
+target.innerHTML = html;
+}
+
+generateAuthors();
+generatePostlinks();
